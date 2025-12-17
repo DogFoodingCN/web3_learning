104a271379b464fc43afe5a7606ee8910bc2775a
AllenZhang <lenz008@qq.com>
2025-12-17 22:12:50 +0800

    first sol with test file

---
+++ hardhat/first_tour/contracts/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Counter {
+    uint public x;
+
+    event Increment(uint by);
+
+    function inc() public {
+//        x += 2; // bug;
+        x += 1; // bug fixed;
+        emit Increment(1);
+    }
+
+    function incBy(uint by) public {
+        require(by > 0, "by must be positive");
+        x += by;
+        emit Increment(by);
+    }
+}
