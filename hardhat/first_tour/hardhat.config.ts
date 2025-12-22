bee77003f3c44bff536401131a4c861a997dc173
lenz008@qq.com <lenz008@qq.com>
2025-12-22 18:42:24 +0800

    tmp_storage

--- hardhat/first_tour/hardhat.config.ts
+++ hardhat/first_tour/hardhat.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "hardhat/config";
+import {configVariable, defineConfig} from "hardhat/config";
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import hardhatVerify from "@nomicfoundation/hardhat-verify";
 
@@ -13,16 +13,21 @@ export default defineConfig({
     networks: {
         sepolia: {
             type: "http",
-            url:"https://rpc.sepolia.org",
+            chainType: "l1",
+            // chainId: 11155111 ,
+            // url: configVariable("SEPOLIA_RPC_URL"),
+            // accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+            url:"https://sepolia.infura.io/v3/ddeaff56b8f847918cb455b4f62f1fe5",
             accounts:["0xe22a2b8c0825fc95390578bfdbcd8cf9311932e16bbd1d22e3bf5b1d61fdd34a"],
         },
     },
     verify: {
         etherscan: {
+            // apiKey: configVariable("ETHERSCAN_API_KEY"),
             apiKey: "TCU9NJPQC7MMM15PR34H31Z9ABP95G7Q6J",
         },
-        blockscout: {
-            enabled: false,
-        }
+        // blockscout: {
+        //     enabled: false,
+        // }
     }
 });
